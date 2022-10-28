a21a694c88ecf4dbe56655ef641d9789221ce6e6
pannawat05 <83331747+pannawat05@users.noreply.github.com>
2022-10-28 19:14:11 +0700

    setup_1

--- conn.php
+++ conn.php
@@ -1,8 +1,8 @@
 <?php
-$host='sql12.freemysqlhosting.net';
-$user='sql12529849';
-$password='quaW6uyygH';
-$database='sql12529849';
+$host='phpMyAdmin demo-MySQL';
+$user='root';
+$password='';
+$database='10_28';
  $con= mysqli_connect($host,$user,$password,$database);
  error_reporting(0);
 
