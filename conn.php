ca07586e9ecb9aed0c9d3eefd645a752971161e5
pannawat05 <83331747+pannawat05@users.noreply.github.com>
2022-10-28 18:47:08 +0700

    db setup

--- conn.php
+++ conn.php
@@ -1,8 +1,8 @@
 <?php
-$host='localhost';
-$user='root';
-$password='';
-$database='e-commerce';
+$host='sql12.freemysqlhosting.net';
+$user='sql12529849';
+$password='quaW6uyygH';
+$database='sql12529849';
  $con= mysqli_connect($host,$user,$password,$database);
  error_reporting(0);
 
