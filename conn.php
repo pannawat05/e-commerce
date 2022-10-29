f9ec01fd7fffff8ef746a4bcc4dc34db627e8198
pannawat05 <83331747+pannawat05@users.noreply.github.com>
2022-10-29 10:38:59 +0700

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,7 +1,7 @@
 <?php
 $host='db4free.net';
 $user='panplay_it';
-$password='panplay_it';
+$password='pan_05052';
 $database='shop_01';
  $con= mysqli_connect($host,$user,$password,$database);
  error_reporting(0);
