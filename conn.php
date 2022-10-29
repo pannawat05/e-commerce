c4ee6494f4708f5805d938122ef67e8b6ee75b1b
pannawat05 <83331747+pannawat05@users.noreply.github.com>
2022-10-29 13:00:23 +0700

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,8 +1,8 @@
 <?php
-$host='db4free.net';
-$user='panplay_it';
-$password='pan_05052';
-$database='shop_01';
+$host='b73gdo6gxqwun1svbpc6-mysql.services.clever-cloud.com';
+$user='uvmeyd533gimydyy';
+$password='eYJPLDsBaS7HD6F8p9Y9';
+$database='b73gdo6gxqwun1svbpc6';
  $con= mysqli_connect($host,$user,$password,$database);
  error_reporting(0);
 
